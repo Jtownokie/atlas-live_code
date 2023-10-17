954ef60a0826ab3b832588bfc508140318bf2d13
Chris Stephens <6673@holbertonstudents.com>
2023-10-17 14:55:30 -0500

    Update malloc.c

--- malloc_free/malloc.c
+++ malloc_free/malloc.c
@@ -11,10 +11,10 @@
 int *new_int_array(int size, int num)
 {
     // Allocate memory for an integer array and initialize it
-    int *int_array; // What's the difference between *int_array and int_array[]?
+    int *int_array;
     int i;
 
-    int_array = malloc(sizeof(int) * size); // Trivia Question (size of int?)
+    int_array = malloc(sizeof(int) * size);
 
     for (i = 0; i < size; i++)
     {
@@ -41,17 +41,17 @@ char **new_string_array(int size, char *word) // Add double pointer explanation/
 {
     // Allocate memory for an array of strings, as well as each index of
     // the array, then initialize each index
-    char **str_array; // Quick review double pointers (pointer to pointer)
-    int i;           // what does dereferencing do with double pointers?
+    char **str_array;
+    int i;
 
     str_array = malloc(sizeof(char *) * (size + 1)); // Why the + 1?
 
     for (i = 0; i < size; i++)
     {
-        str_array[i] = malloc(sizeof(char) * (strlen(word) + 1)); // Audience Collaboration
-        strcpy(str_array[i], word); // Why strcpy() instead of str_array[i] = word?
+        str_array[i] = malloc(sizeof(char) * (strlen(word) + 1));
+        strcpy(str_array[i], word);
     }
-    str_array[i] = NULL; // What's this for?
+    str_array[i] = NULL;
 
     for (i = 0; i < size; i++)
     {
@@ -60,7 +60,7 @@ char **new_string_array(int size, char *word) // Add double pointer explanation/
 
     for (i = 0; i < size; i++)
     {
-        free(str_array[i]); // Why do we need to free indices when we didn't
-    }                       // have to with the int_array?
+        free(str_array[i]);
+    }
     free(str_array);
 }
