578f3bf02d6b32678873e6c07302cc5f6580d3a6
Jtownokie <christopher.stephens@atlasschool.com>
2024-01-15 21:08:07 -0600

    Completed Template Branch

--- python_inheritance/classes.py
+++ python_inheritance/classes.py
@@ -1,37 +1,13 @@
 # Understanding Inheritance
 
 class Animal:
-    def __init__(self, color):
-        self.color = color
-
-    alive = True
-
-    def eat(self):
-        print("This animal is eating")
-
-    def sleep(self):
-        print("This animal is sleeping")
-
-    def print_color(self):
-        print(f"The {self.__class__.__name__}'s color is {self.color}")
+    pass
 
 class Rabbit(Animal):
-    def __init__(self, color):
-        super().__init__(color)
-
-    def hop(self):
-        print("The Rabbit is hopping")
+    pass
 
 class Fish(Animal):
-    def __init__(self, color):
-        super().__init__(color)
-
-    def swim(self):
-        print("The Fish is swimming")
+    pass
 
 class Hawk(Animal):
-    def __init__(self, color):
-        super().__init__(color)
-
-    def fly(self):
-        print("The Hawk is flying")
+    pass
--- python_inheritance/main.py
+++ python_inheritance/main.py
@@ -2,23 +2,9 @@ from classes import Animal, Rabbit, Fish, Hawk
 
 if __name__ == "__main__":
     # Instantiate Objects
-    animal_obj = Animal("Black")
-    rabbit_obj = Rabbit("Brown")
-    fish_obj = Fish("Blue")
-    hawk_obj = Hawk("Grey")
 
     # Call Animal Class Methods
-    animal_obj.sleep()
-    rabbit_obj.eat()
-    fish_obj.sleep()
-    hawk_obj.eat()
 
     # Class Names Differ Based On Object Type
-    rabbit_obj.print_color()
-    fish_obj.print_color()
-    hawk_obj.print_color()
 
     # SubClass Specific Methods
-    rabbit_obj.hop()
-    fish_obj.swim()
-    hawk_obj.fly()
